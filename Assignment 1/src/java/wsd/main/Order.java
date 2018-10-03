336a1320182ea01d1abcabc20cb5a66b08894280
Goodguyswiftie <philipjohan.aubert@student.uts.edu.au>
2018-10-03 22:20:33 +1000

    started Order class

--- Assignment 1/src/java/wsd/main/Order.java
+++ Assignment 1/src/java/wsd/main/Order.java
@@ -19,18 +19,64 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name="user")
 public class Order implements Serializable {
     @XmlElement(name = "IDint")
-    private int IDint;
+    private int IDint = 0 + (int) (Math.random() * ((999) + 1));
     @XmlElement(name = "ID")
     private String ID = String.format("%3d", IDint);
-    @XmlElement(name = "title")
-    private String title;
-    @XmlElement(name = "release_date")
-    private String release_date;
-    @XmlElement(name = "genre")
-    private String genre;
-    @XmlElement(name = "price")
-    private String price;
-    @XmlElement(name = "available_copies")
-    private String available_copies;
+    @XmlElement(name = "copies_purchased")
+    private int copies_purchased;
+    @XmlElement(name = "user")
+    User user = new User();
+    @XmlElement(name = "movies")
+    Movies movies = new Movies();
     
+    @XmlElement(name = "user_full_name")
+    private String user_full_name;
+    @XmlElement(name = "user_name")
+    private String user_name;
+    @XmlElement (name = "payment_method")
+    private String payment_method;
+    @XmlElement (name = "status")
+    private String status = "submitted";
+    
+    public Order(User user, Movie movie, int quantity, String paymentMethod) {
+        this.user = user;
+        addMovie(movie, quantity);
+        this.payment_method = paymentMethod;
+        this.status = "submitted";
+        
+    }
+    public Order() {
+        
+    }
+    public String getID() {
+        return ID;
+    }
+    public Movies getMovies() {
+        return movies;
+    }
+    public String getFullName() {
+        return user.getName();
+    }
+    public String getUserName() {
+        return user.getEmail();
+    }
+    public String getSaleTotal() {
+        int saleTotal = 0;
+        for (Movie movie : movies.getList()) {
+            saleTotal += Integer.parseInt(movie.getAvailable_copies());
+        }
+        return String.valueOf(saleTotal);
+    }
+    public String getStatus() {
+        return status;
+    }
+    
+    public void cancelStatus() {
+        status = "cancelled";
+    }
+    
+    public void addMovie(Movie movie, int quantity) {
+        movie.setAvailable_copies(String.valueOf(quantity));
+        movies.addMovie(movie);
+    }
 }
